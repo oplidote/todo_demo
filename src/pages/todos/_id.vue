186e58d9d6810a4512aa7c9c0f83cc3a7d1fe150
jhwmac <jkr2505@naver.com>
2022-04-11 14:31:55 +0900

    router 세팅

---
+++ src/pages/todos/_id.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>Todo</div>
+</template>
+<script>
+import {useRoute} from 'vue-router'
+export default {
+    setup() {
+        const route = useRoute();
+        console.log(route.params.id);
+    }
+}
+</script>
